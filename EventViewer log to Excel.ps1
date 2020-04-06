8bb5f5b5dca8555d96fc4850b7545bd0338ce54a
Purushottam Gupta <purushottam.2014@vit.ac.in>
2020-04-06 16:14:19 +0530

    Add files via upload

---
+++ EventViewer log to Excel.ps1
@@ -0,0 +1,45 @@
+﻿# Getting the latest top 10 EventLogs for Source named CloudFlare from Application
+$CFLog = Get-EventLog -LogName Application -Source "Application Error" 
+$CFLog | Select-Object -Property *
+
+#Excel Com object
+$Excel=new-object -ComObject Excel.Application
+$Excel.Visible = $True  # setting visibility to true
+
+#adding and and selecting new excel page
+$xl = $Excel.Workbooks.Add()
+#$myXl = $Excel.Selection
+$myXlSheet = $Excel.Worksheets.Item("sheet1")
+$myXlSheet.activate()
+
+$myXlSheet.Cells.Item(1,1) = "Time Generated"
+$myXlSheet.Cells.Item(1,2) = "Machine Name"
+$myXlSheet.Cells.Item(1,3) = "Entry Type"
+$myXlSheet.Cells.Item(1,4) = "Source"
+$myXlSheet.Cells.Item(1,5) = "Message"
+
+$row1 = 2;
+
+
+foreach($item in $CFLog){
+IF($item){
+    $myXlSheet.Cells.Item($row1,1) = $item.TimeGenerated
+    $myXlSheet.Cells.Item($row1,2) = $item.MachineName
+    $myXlSheet.Cells.Item($row1,3) = $item.EntryType
+    $myXlSheet.Cells.Item($row1,4) = $item.Source
+    $myXlSheet.Cells.Item($row1,5) = $item.Message
+    }
+    $row1++
+    }
+
+
+# Set the width of the columns automatically
+$myXlSheet.columns.item("A:J").EntireColumn.AutoFit() | out-null
+
+$Excel.DisplayAlerts = 'True'
+$ext=".xlsx"
+$path="D:\Logs\CloudFlareLogs$ext"
+$myXlSheet.SaveAs($path) 
+$myXlSheet.Close
+$Excel.DisplayAlerts = 'True'
+$Excel.Quit()
